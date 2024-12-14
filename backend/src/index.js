cfdf028aa80aafa4161e9542176ba1e4f5fed102
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-14 20:03:03 +0530

    Utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,13 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path: './env'});
 
-connectDB();
+connectDB()
+.then(()=> {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => `Server running on port ${port}`);
+})
+.catch((err)=> {
+    console.log("MongoDb connection failed !! ");
+});
 
 
