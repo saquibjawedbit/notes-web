46a19ff02842299e1307fed706c9b6c1c55cffb2
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-19 03:24:31 +0530

    Refactor user registration route and add new endpoints for password change, user details retrieval, and account updates

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,21 +5,15 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1,
-        }
-    ]),
-    registerUser
-);
-
+router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 
 //secured routes
 router.route("/logout").post(verifyJWT ,logoutUser);
 router.route("/refresh").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/me").get(verifyJWT, getCurrentUser);
+router.route("/update-account").put(verifyJWT, updateAccountDetails);
 
 export default router;
