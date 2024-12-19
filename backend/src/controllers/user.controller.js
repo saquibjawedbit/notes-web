79b0d50b6491d5d0c694c8bee812da3fb156b930
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-19 13:55:10 +0530

    Remove refresh token on user logout by unsetting the field in the database

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -109,8 +109,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined,
+            $unset: {
+                refreshToken: 1,
             }
         },
         {
