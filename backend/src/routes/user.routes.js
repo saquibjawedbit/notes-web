005617273370eccc868903d33962efa0108642fc
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-19 14:23:29 +0530

    Remove unused import statement for multer middleware in user routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,5 @@
 import {Router} from "express";
 import {registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, changeCurrentUserPassword, updateAccountDetails} from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
