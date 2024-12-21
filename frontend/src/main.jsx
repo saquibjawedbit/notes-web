a90fe4a03355e41a9a40cf1bc7e0ca39cb6f9b90
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-21 23:21:23 +0530

    Refactor routing in main.jsx to include Switch component for improved route management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router';
 import './index.css'
 import App from './pages/landing/App.jsx'
 import { Layout } from './Layout.jsx';
