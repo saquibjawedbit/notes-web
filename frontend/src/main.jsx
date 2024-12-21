3d9504044a69373814266e517a28f1b8f75e21ed
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-21 23:20:24 +0530

    Refactor routing structure in main.jsx to use Switch component for better route handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,15 +8,15 @@ import { Home } from './pages/home/Home.jsx';
 import { AboutUs } from './components/about-us/AboutUs.jsx';
 import { Login } from './pages/authentication/Login.jsx';
 import { Layout as AuthLayout } from './pages/authentication/common/Layout.jsx';
-import {SignUp} from './pages/authentication/SignUp.jsx';
-import {ForgotPassword} from './pages/authentication/ForgotPassword.jsx';
+import { SignUp } from './pages/authentication/SignUp.jsx';
+import { ForgotPassword } from './pages/authentication/ForgotPassword.jsx';
 import Chapters from './pages/chapters/Chapters.jsx';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import axios from "axios";
 
 
-  
-  
+
+
 if (import.meta.env.MODE === 'production') {
   // Safely remove React DevTools in production
   axios.defaults.withCredentials = true;
@@ -27,19 +27,21 @@ if (import.meta.env.MODE === 'production') {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<App />} />
-        <Route path='/about-us' element={<AboutUs />}/>
-        <Route path='/notes' element={<Home />}/>
-        <Route path='/notes/:subject/:chapter' element={<Chapters/>} />
-      </Route>
-      <Route path='/' element={<AuthLayout/>}>
-        <Route path='login' element={<Login/>}/>
-        <Route path='signup' element={<SignUp/>}/>
-        <Route path='forgotPassword' element={<ForgotPassword/>}/>
-      </Route>
-    </Routes>
+      <Switch>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<App />} />
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/notes' element={<Home />} />
+            <Route path='/notes/:subject/:chapter' element={<Chapters />} />
+          </Route>
+          <Route path='/' element={<AuthLayout />}>
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<SignUp />} />
+            <Route path='forgotPassword' element={<ForgotPassword />} />
+          </Route>
+        </Routes>
+      </Switch>
     </BrowserRouter>
   </StrictMode>,
 )
