f5260a02de9f99cb576c9bda46e63d76cc681475
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-22 15:38:03 +0530

    Remove connection verification from email transporter utility

--- backend/src/utils/emailTransporter.js
+++ backend/src/utils/emailTransporter.js
@@ -12,18 +12,6 @@ const sendEmail = (mailOptions) => {
             }
         });
 
-        await new Promise((resolve, reject) => {
-            // verify connection configuration
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                } else {
-                    console.log("Server is ready to take our messages");
-                    resolve(success);
-                }
-            });
-        });
 
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
