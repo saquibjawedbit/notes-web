47171a22af32500068f8545c6b51394c6c4556eb
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-22 15:46:18 +0530

    Refactor email sending function to use async/await and add connection verification

--- backend/src/utils/emailTransporter.js
+++ backend/src/utils/emailTransporter.js
@@ -1,27 +1,40 @@
 import nodemailer from 'nodemailer';
 
-const sendEmail = (mailOptions) => {
-    return new Promise(async (resolve, reject) => {
-        const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST || "smtp.gmail.com",
-            port: process.env.SMTP_PORT || 465,
-            secure: process.env.SMTP_SECURE === 'true' || true, // true for 465, false for other ports
-            auth: {
-                user: process.env.SMTP_EMAIL,
-                pass: process.env.SMTP_PASSWORD,
-            }
-        });
+const sendEmail = async (mailOptions) => {
 
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST || "smtp.gmail.com",
+        port: process.env.SMTP_PORT || 465,
+        secure: process.env.SMTP_SECURE === 'true' || true, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD,
+        }
+    });
 
-        transporter.sendMail(mailOptions, (error, info) => {
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
             if (error) {
-                console.error("Email Error:", error);
-                resolve(false); // or reject(error) if you want to handle errors
+                console.log(error);
+                reject(error);
             } else {
-                console.log('Email sent: ' + info.response);
-                resolve(true);
+                console.log("Server is ready to take our messages");
+                resolve(success);
             }
         });
+    });
+    
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error("Email Error:", error);
+            resolve(false); // or reject(error) if you want to handle errors
+        } else {
+            console.log('Email sent: ' + info.response);
+            resolve(true);
+        }
     });
 };
 
