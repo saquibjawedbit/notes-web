8204b2556b73d663006c8f9f57288cd0113c987c
Md Saquib Jawed <saquibjawed4444@gmail.com>
2024-12-31 21:54:41 +0530

    Update PDF.js worker source in PdfViewer component and comment out local worker script in index.html

--- frontend/src/pages/pdfViewer/PdfViewer.jsx
+++ frontend/src/pages/pdfViewer/PdfViewer.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useParams } from 'react-router';
 import ReactLoading from 'react-loading';
 
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
 
 const PdfViewer = () => {
