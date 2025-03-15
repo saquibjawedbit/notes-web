eaee7989ef0cb8196464c4c745db95da95cc9ab9
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-03-15 13:56:29 +0530

    Update CORS configuration to include additional allowed origins and enhance request handling

--- backend/src/configs/corsOption.js
+++ backend/src/configs/corsOption.js
@@ -1,17 +1,20 @@
-const allowedOrigins = (process.env.NODE_ENV === "dev") 
+const allowedOrigins = (process.env.NODE_ENV === "dev")
   ? ['http://localhost:5173'] // Allow all in development
-  : [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2, process.env.CORS_ORIGIN_3]; 
+  : [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2, process.env.CORS_ORIGIN_3, 'https://www.hrsciencequest.com'];
 
 const corsOption = {
-    origin: 
-      (origin, callback) => {
-          if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true); // Allow request
-          } else {
-            callback(new Error('Not allowed by CORS')); // Deny request
-          }
-        },
-    credentials: true // Allow credentials like cookies
+  origin:
+    (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Deny request
+      }
+    },
+  credentials: true, // Allow credentials like cookies
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range']
 };
 
 export default corsOption;
