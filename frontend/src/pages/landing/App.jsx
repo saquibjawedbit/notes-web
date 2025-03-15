965d3b20aafcf7d6b300a5efb3e2607bb577a9ed
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-03-15 14:08:41 +0530

    Add console log for full testimonials response in App component

--- frontend/src/pages/landing/App.jsx
+++ frontend/src/pages/landing/App.jsx
@@ -17,6 +17,7 @@ function App() {
     axios.get('/api/v1/testimonials')
       .then(response => {
         console.log(response.data.data);
+        console.log(response.data);
         if(response.data.data)
         setTestimonials(response.data.data);
       })
